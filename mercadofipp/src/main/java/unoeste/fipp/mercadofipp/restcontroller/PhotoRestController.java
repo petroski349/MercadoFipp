d441429cfe10bf99f7fa187286be120c67dae280
Diogo Petroski <diogopetroski@gmail.com>
2024-11-21 18:49:31 -0300

    ss

--- mercadofipp/src/main/java/unoeste/fipp/mercadofipp/restcontroller/PhotoRestController.java
+++ mercadofipp/src/main/java/unoeste/fipp/mercadofipp/restcontroller/PhotoRestController.java
@@ -2,6 +2,7 @@ package unoeste.fipp.mercadofipp.restcontroller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import unoeste.fipp.mercadofipp.db.entity.Photo;
 import unoeste.fipp.mercadofipp.db.repository.PhotoRepository;
 import unoeste.fipp.mercadofipp.service.PhotoService;
@@ -46,5 +47,28 @@ public class PhotoRestController {
             return ResponseEntity.ok(photo);
         else
             return ResponseEntity.badRequest().body("Erro");
+    }
+
+    @PostMapping(value="add")
+    public ResponseEntity<Object> add(String nome, String tipo, String nivel, MultipartFile imagem)
+    {
+        Pokemon pokemon=null;
+        boolean erro=false;
+        try{
+            String filename=""+(new Date().getTime())+imagem.getOriginalFilename().substring(imagem.getOriginalFilename().lastIndexOf("."));
+            Files.copy(imagem.getInputStream(), Paths.get(getStaticPath()).resolve(filename));
+            pokemon=new Pokemon(nome,tipo, Integer.parseInt(nivel),"");
+            pokemon.setImagem(filename);
+            pokeList.add(pokemon);
+        }
+        catch(Exception e){
+            System.out.println(e);
+            erro=true;
+        }
+
+        if(!erro)
+            return ResponseEntity.ok(pokemon);
+        else
+            return ResponseEntity.badRequest().body("Não é possível cadastrar o pokemon");
     }
 }
